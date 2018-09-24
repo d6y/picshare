d36578cab0f4071e26baf9a8db39642d103cafc3
Richard Dallaway <richard@dallaway.com>
2018-09-24 12:02:25 +0100

    elm format source

--- src/Picshare.elm
+++ src/Picshare.elm
@@ -1,8 +1,8 @@
 module Picshare exposing (main)
 
 import Browser
-import Html exposing (Html, form, input, button, div, h1, h2, i, img, text, strong, ul, li)
-import Html.Attributes exposing (class, placeholder, href, src, type_, disabled, value)
+import Html exposing (Html, button, div, form, h1, h2, i, img, input, li, strong, text, ul)
+import Html.Attributes exposing (class, disabled, href, placeholder, src, type_, value)
 import Html.Events exposing (onClick, onInput, onSubmit)
 
 
@@ -10,7 +10,7 @@ type alias Photo =
     { url : String
     , caption : String
     , liked : Bool
-    , comments: List String
+    , comments : List String
     , newComment : String
     }
 
@@ -36,21 +36,20 @@ update msg model =
     case msg of
         ToggleLike ->
             { model | liked = not model.liked }
-        
+
         UpdateComment comment ->
-           { model | newComment = comment }
+            { model | newComment = comment }
 
         SaveComment ->
-            case (String.trim model.newComment) of
-                "" -> 
+            case String.trim model.newComment of
+                "" ->
                     model
 
-                comment -> 
-                    { model 
-                        | comments = model.comments ++ [comment]
+                comment ->
+                    { model
+                        | comments = model.comments ++ [ comment ]
                         , newComment = ""
                     }
-            
 
 
 viewLoveButton : Photo -> Html Msg
@@ -59,6 +58,7 @@ viewLoveButton model =
         buttonClass =
             if model.liked then
                 "fa-heart"
+
             else
                 "fa-heart-o"
     in
@@ -71,24 +71,27 @@ viewLoveButton model =
             []
         ]
 
+
 viewComment : String -> Html Msg
 viewComment comment =
     li []
-        [ strong [] [ text "Comment:"]
-        , text ( " " ++ comment)
+        [ strong [] [ text "Comment:" ]
+        , text (" " ++ comment)
         ]
+
 
 viewCommentList : List String -> Html Msg
 viewCommentList comments =
     case comments of
         [] ->
             text ""
-        
+
         _ ->
             div [ class "comments" ]
                 [ ul []
                     (List.map viewComment comments)
                 ]
+
 
 viewComments : Photo -> Html Msg
 viewComments model =
@@ -102,9 +105,9 @@ viewComments model =
                 , onInput UpdateComment
                 ]
                 []
-             , button 
-                [ disabled (String.isEmpty model.newComment) ] 
-                [text "Save"]
+            , button
+                [ disabled (String.isEmpty model.newComment) ]
+                [ text "Save" ]
             ]
         ]
 
